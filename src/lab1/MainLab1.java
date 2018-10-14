6bebe429e7e7aab04d3513c8eb1d7b6f275168f3
Jared R. Mallas <jrmallas@icloud.com>
2018-10-14 14:50:31 -0500

    couple of todos

--- src/lab1/MainLab1.java
+++ src/lab1/MainLab1.java
@@ -3,6 +3,12 @@ package lab1;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Main class to demonstrate working with abstract classes
+ * TODO: Explain why I hate abstract classes
+ * @author Jared Mallas
+ * @version 1.0
+ */
 public class MainLab1
 {
     public static void main(String[] args)
@@ -13,7 +19,7 @@ public class MainLab1
         {
             courses.add(new IntroToProgrammingCourse("Introduction to programming", "100-025", 5d, ""));
             courses.add(new IntroJavaCourse("Introduction to java", "100-050", 4d, "Introduction to Programming"));
-            courses.add(new AdvancedJavaCourse("Advanced java", "100-100", 5d, "Introduction to Java"));
+            courses.add(new AdvancedJavaCourse("Advanced java", "100-100", 4d, "Introduction to Java"));
         }
         catch (IllegalArgumentException ex)
         {
