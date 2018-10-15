7a948844eb9aa597c6053fc2936c0ea427f5db55
Jared R. Mallas <jrmallas@icloud.com>
2018-10-15 16:33:59 -0500

    moar tests for much win

--- src/org/jrm/test/lab2/AdvancedJavaCourseTest.java
+++ src/org/jrm/test/lab2/AdvancedJavaCourseTest.java
@@ -2,6 +2,7 @@ package org.jrm.test.lab2;
 
 import lab2.AdvancedJavaCourse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,9 +24,21 @@ class AdvancedJavaCourseTest
         assertNotNull(ajc2);
     }
 
+    @Test
+    void setPrerequisites()
+    {
+        assertThrows(IllegalArgumentException.class, () -> ajc2.setPrerequisites(null), "Prereqs cant be null");
+    }
+
     @org.junit.jupiter.api.Test
     void getCourseName() {
         assertEquals("Advanced Java", ajc2.getCourseName(), "Name should be Advanced Java");
+    }
+
+    @Test
+    void setCourseName() {
+        ajc2.setCourseName("some course");
+        assertEquals("some course", ajc2.getCourseName(), "Set course name works!");
     }
 
     @org.junit.jupiter.api.Test
@@ -33,10 +46,24 @@ class AdvancedJavaCourseTest
         assertEquals("100-099", ajc2.getCourseNumber(), "Number should be 100-099");
     }
 
+    @Test
+    void setCourseNumber()
+    {
+        ajc2.setCourseNumber("123-456-789");
+        assertEquals("123-456-789", ajc2.getCourseNumber(), "get course number works");
+    }
+
+    @Test
+    void getCredits()
+    {
+        Double x = 2d;
+        ajc2.setCredits(x);
+        assertEquals(x, ajc2.getCredits(), "get credits works");
+    }
+
     @org.junit.jupiter.api.Test
     void setCredits() {
         assertThrows(IllegalArgumentException.class, () -> ajc2.setCredits(5d), "Credits cannot exceed 4.0");
         assertThrows(IllegalArgumentException.class, () -> ajc2.setCredits(.4d), "Credits cannot be less than .5");
     }
-
 }
